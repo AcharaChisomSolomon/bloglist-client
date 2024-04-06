02b1c66409d1e28e4dc712f6a034f673b456f5c9
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-04-06 13:31:36 +0100

    Update App.jsx to fetch and display blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-const App = () => { 
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    async function fetchBlogs() { 
+      const blogs = await blogService.getAll()
+      setBlogs(blogs)
+    }
+    fetchBlogs()
+   }, [])
+
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>Welcome to the React 18 Alpha</p>
+      <h2>Blogs</h2>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
   )
 }
