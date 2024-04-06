6827bd18842b69b1a45ce1e8aba11623d60a5a18
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-04-06 13:27:29 +0100

    Add axios dependency and update App.jsx and App.css

---
+++ src/components/Blog.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const Blog = ({ blog }) => { 
+    return (
+        <div>
+            {blog.title} {blog.author}
+        </div>
+    );
+};
+
+export default Blog;
